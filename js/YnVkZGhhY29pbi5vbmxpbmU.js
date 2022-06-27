9eaafba82980165e8dec8eb2afcf309b66f6a3df
buddhacoingithub <106426503+buddhacoingithub@users.noreply.github.com>
2022-06-27 17:09:39 +0800

    Create YnVkZGhhY29pbi5vbmxpbmU.js

---
+++ js/YnVkZGhhY29pbi5vbmxpbmU.js
@@ -0,0 +1,1 @@
+var head,link;"buddhacoin.online"==window.location.hostname&&(head=document.getElementsByTagName("HEAD")[0],(link=document.createElement("link")).rel="stylesheet",link.type="text/css",link.href="css/YnVkZGhhY29pbi5vbmxpbmU.css",head.appendChild(link));
